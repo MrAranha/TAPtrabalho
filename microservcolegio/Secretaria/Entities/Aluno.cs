e56f90bc49041073dc22ed75b82731f063440e1c
Breno Aranha <aranha.breno53@gmail.com>
2024-11-06 21:10:16 -0300

    s

---
+++ microservcolegio/Secretaria/Entities/Aluno.cs
@@ -0,0 +1,12 @@
+namespace microservcolegio.Secretaria.Entities;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class Aluno
+{
+    public Guid id {get;set;}
+    public string name {get;set;}
+}
