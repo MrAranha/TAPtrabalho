772a0ada44a8758b8394bb243702016d710724da
Breno Aranha <101605425+MrAranha@users.noreply.github.com>
2024-12-04 18:28:16 +0000

    d

--- microservcolegio/Secretaria/Entities/RepositoryDbContext.cs
+++ microservcolegio/Secretaria/Entities/RepositoryDbContext.cs
@@ -1,45 +1,55 @@
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Azure.Cosmos;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.ValueGeneration;
 
 namespace microservcolegio.Secretaria.Entities;
+
 public class RepositoryDbContext : DbContext
 {
-    public DbSet<Aluno> Alunos {get;set;}
     private IConfiguration _configuration;
+    public DbSet<Aluno> Alunos {get;set;}
+
     public RepositoryDbContext(IConfiguration configuration){
         this._configuration = configuration;
     }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+
         optionsBuilder.UseCosmos(
             connectionString: this._configuration["CosmosDBURL"],
             databaseName: this._configuration["CosmosDBDBName"],
             cosmosOptionsAction: options =>
             {
                 options.ConnectionMode(ConnectionMode.Gateway);
-                /*options.HttpClientFactory(() => new HttpClient(new HttpClientHandler()
+                options.HttpClientFactory(() => new HttpClient(new HttpClientHandler()
                 {
                     ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-                }));*/
+                }));
             }
 
         );
     }
-    protected override void OnModelCreating(ModelBuilder modelBuilder){
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
         modelBuilder.Entity<Aluno>()
-            .HasNoDiscriminator();
-        modelBuilder.Entity<Aluno>()
-            .ToContainer("aluno");
-        modelBuilder.Entity<Aluno>()
-            .Property(p=>p.id)
-            .HasValueGenerator<GuidValueGenerator>();
-        modelBuilder.Entity<Aluno>()
-            .HasPartitionKey(p => p.id);
+            .ToContainer("aluno")
+            .HasPartitionKey(p => p.id)
+            .HasAutoscaleThroughput(400)
+            .HasNoDiscriminator()
+            .Property(p => p.id)
+            .HasValueGenerator<GuidValueGenerator>()
+            .IsRequired(true)
+            ;
+
+
+        
+
     }
 
 }
